b505db00cc9664930de54423419aecccb0384e75
Andrea Sveinsdottir <121334896+Guppie88@users.noreply.github.com>
2024-09-07 10:30:21 +0200

    Initial commit

---
+++ src/test/java/achat/achat/AchatApplicationTests.java
@@ -0,0 +1,13 @@
+package achat.achat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
