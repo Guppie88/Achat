6b4c966f699c77e8ebf35815176e478a81b534d2
Andrea Sveinsdottir <121334896+Guppie88@users.noreply.github.com>
2024-09-07 14:53:35 +0200

    Projektstart AchatApplication.java

--- src/main/java/achat/achat/AchatApplication.java
+++ src/main/java/achat/achat/AchatApplication.java
@@ -2,12 +2,11 @@ package achat.achat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class})
 public class AchatApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AchatApplication.class, args);
     }
-
 }
